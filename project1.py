9add57e043f985f52f933ae08cdafbc9674b68aa
Jake Dibble <jdibble@bsu.edu>
2021-03-01 16:08:20 -0500

    Complete normalize function

--- project1.py
+++ project1.py
@@ -1,19 +1,35 @@
 import pandas as pd
+pd.set_option('display.max_rows', None)
 import numpy as np
 import math
 
 class ClusterData():
     def __init__(self,df1,df2):
         self.dataframe = pd.read_csv("cs455_homework3_dataset_dibble.csv")
-        self.numericDataFrame1 = self.dataframe[df1]
-        self.numericDataFrame2 = self.dataframe[df2]
+        self.normalizedDataFrame1 = self.dataframe[df1]
+        self.normalizedDataFrame2 = self.dataframe[df2]
         self.k = self.getKValue()
 
-    def normalize():
-        pass
-    def getKValue():
+    def normalize(self,df):
+        # Normalize values in both dataframes (0 - 100)
+        df1 = self.normalizedDataFrame1
+        df1 = ((df1-df1.min())/(df1.max()-df1.min()))*100
+        self.normalizedDataFrame1 = df1
+
+        df2 = self.normalizedDataFrame2
+        df2 = ((df2-df2.min())/(df2.max()-df2.min()))*100
+        self.normalizedDataFrame2 = df2
+
+    def getKValue(self):
         kVal = 0
-        while kVal < 1 and kVal > 5:
+        while (True):
             kVal = int(input("Enter an integer between 2 and 4 for the k value: "))
-            if(kVal < 1 and kVal > 5):
-                print("ERROR: k value must be an integer between 2 and 4")
+            if(kVal > 1 and kVal < 5):
+                break
+            print("ERROR: k value must be an integer between 2 and 4")
+        return kVal
+
+    def distance(self,p, q):
+        return int(math.fabs(p[0] - q[0]) + math.fabs(p[1]-q[1]))
+
+clusterMapping = ClusterData('white_rating','black_rating')
