797f4a58d4b3efd90524a2e97a49a13a68045b23
Jake Dibble <jdibble@bsu.edu>
2021-03-01 19:25:29 -0500

    Add sklearn and matplotlib to calculate centroid and display result

--- project1.py
+++ project1.py
@@ -1,17 +1,21 @@
 import pandas as pd
 pd.set_option('display.max_rows', None)
-import numpy as np
 import math
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 class ClusterData():
     def __init__(self,df1,df2):
+        self.df1Name = df1
+        self.df2Name = df2
         self.dataframe = pd.read_csv("cs455_homework3_dataset_dibble.csv")
         self.normalizedDataFrame1 = self.dataframe[df1]
         self.normalizedDataFrame2 = self.dataframe[df2]
+        self.combinedDataFrame = []
         self.k = self.getKValue()
 
     def normalize(self):
-        # Normalize values in both dataframes (0 - 100)
+        # Normalize values in both dataframes (0 - 100), cast to integers
         df1 = self.normalizedDataFrame1
         df1 = ((df1-df1.min())/(df1.max()-df1.min()))*100
         df1 = df1.astype(int)
@@ -21,6 +25,9 @@ class ClusterData():
         df2 = ((df2-df2.min())/(df2.max()-df2.min()))*100
         df2 = df2.astype(int)
         self.normalizedDataFrame2 = df2
+
+        # Combine dataframes into single 2 column frame
+        self.combinedDataFrame = pd.concat([df1,df2], join='outer',axis=1)
 
     def getKValue(self):
         kVal = 0
@@ -34,6 +41,22 @@ class ClusterData():
     def distance(self,p, q):
         return int(math.fabs(p[0] - q[0]) + math.fabs(p[1]-q[1]))
 
-clusterMapping = ClusterData('white_rating','black_rating')
-clusterMapping.normalize()
+    def SSE(self,center, cluster):
+        sum = 0
+        for p in cluster:
+            d = self.distance(center, p)
+            sum = sum + d * d
+        return sum
+
+cMap = ClusterData('white_rating','black_rating')
+cMap.normalize()
+
+kmeans = KMeans(n_clusters=cMap.k).fit(cMap.combinedDataFrame)
+centroids = kmeans.cluster_centers_
+print(centroids)
+
+plt.scatter(cMap.combinedDataFrame[cMap.df1Name],cMap.combinedDataFrame[cMap.df2Name],  s=50, alpha=0.5)
+plt.scatter(centroids[:, 0],centroids[:, 1], c='red',s=50)
+plt.show()
+
 
